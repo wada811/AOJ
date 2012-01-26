57072a013b5d96678daa2cc5c00e085d569bae7c
wada811 <89.at.usi@gmail.com>
2012-01-26 21:40:29 +0900

    同上

--- vol100/AOJ10002.cpp
+++ vol100/AOJ10002.cpp
@@ -1,10 +1,10 @@
-/*
+//*
 #include <stdio.h>
 
 int main(){
-	int a, b;
-	scanf("%d %d", &a, &b);
-	printf("%d %d\n", a*b, 2*a+2*b);
-	return 0;
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d %d\n", a*b, 2*a+2*b);
+    return 0;
 }
 //*/
