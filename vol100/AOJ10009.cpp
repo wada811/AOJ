d54a33c840ec785225bc17b546b9a7865b48779f
wada811 <89.at.usi@gmail.com>
2012-01-26 21:48:06 +0900

    同上

--- vol100/AOJ10009.cpp
+++ vol100/AOJ10009.cpp
@@ -1,10 +1,10 @@
-/*
+//*
 #include <stdio.h>
 
 int main(){
-	double r;
-	scanf("%lf", &r);
-	printf("%f %f\n", 3.141592653589*r*r, 3.141592653589*2*r);
-	return 0;
+    double r;
+    scanf("%lf", &r);
+    printf("%f %f\n", 3.141592653589*r*r, 3.141592653589*2*r);
+    return 0;
 }
 //*/
