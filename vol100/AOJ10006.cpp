1dc7686339355d933db480eafe0b33f7ccc5fdaf
wada811 <89.at.usi@gmail.com>
2012-01-26 21:33:59 +0900

    first commit

---
+++ vol100/AOJ10006.cpp
@@ -0,0 +1,14 @@
+/*
+#include <stdio.h>
+
+int main(){
+	int x;
+	int i=0;
+	while(1){
+		scanf("%d", &x);
+		if(x==0)break;
+		printf("Case %d: %d\n", ++i, x);
+	}
+	return 0;
+}
+//*/
---
+++ vol100/AOJ10008.cpp
@@ -0,0 +1,14 @@
+/*
+#include <stdio.h>
+
+int main(){
+	int a, b, d, r;
+	double f;
+	scanf("%d %d", &a, &b);
+	d=a/b;
+	r=a%b;
+	f=(double)a/(double)b;
+	printf("%d %d %f\n", d, r, f);
+	return 0;
+}
+//*/
