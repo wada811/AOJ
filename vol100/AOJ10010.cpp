c241a50f2d2394179878e8af2bd814cae26b04f6
wada811 <89.at.usi@gmail.com>
2012-01-26 21:50:01 +0900

    同上

--- vol100/AOJ10010.cpp
+++ vol100/AOJ10010.cpp
@@ -5,17 +5,17 @@ int main(){
     int a, b;
     char op;
     while(1){
-    	scanf("%d %c %d", &a, &op, &b);
-    	if(op=='?'){
-    		break;
+        scanf("%d %c %d", &a, &op, &b);
+        if(op=='?'){
+            break;
     	}else if(op=='+'){
-    		printf("%d\n", a+b);
+    	    printf("%d\n", a+b);
     	}else if(op=='-'){
-    		printf("%d\n", a-b);
+    	    printf("%d\n", a-b);
     	}else if(op=='*'){
-    		printf("%d\n", a*b);
+    	    printf("%d\n", a*b);
     	}else if(op=='/'){
-    		printf("%d\n", a/b);
+    	    printf("%d\n", a/b);
         }
     }
     return 0;
