ae05ac2f70cfa3fcf10bbfcadfd31395ee34abaf
wada811 <89.at.usi@gmail.com>
2012-02-06 22:21:54 +0900

    pass AOJ

--- vol100/AOJ10033.cpp
+++ vol100/AOJ10033.cpp
@@ -7,8 +7,10 @@
 
 void push(int n, char stack[Mt][MAX], char c){
     int i;
-    for(i=MAX;i>0;i--){
-        stack[n-1][i]=stack[n-1][i-1];
+    for(i=MAX-1;i>0;i--){
+        if(stack[n-1][i-1]!='\n'){
+            stack[n-1][i]=stack[n-1][i-1];
+        }
     }
     stack[n-1][0]=c;
 }
@@ -17,25 +19,19 @@ char pop(int n, char stack[Mt][MAX]){
     int i;
     char c;
     c=stack[n-1][0];
-    for(i=1;i<MAX;i++){
+    for(i=1;stack[n-1][i-1]!='\n';i++){
         stack[n-1][i-1]=stack[n-1][i];
     }
     return c;
 }
-/*
-void show(int n, char stack[Mt][MAX]){
-    int i;
-    for(i=0;i<MAX;i++){
-        printf("%d:%c\n", i, stack[n-1][i]);
-    }
-    
-}
-*/
+
 int main(){
     int n, m;
     char cmnd[10], stack[Mt][MAX], pops[Mt*MAX];
     char c;
     int i, j=0;
+
+    memset(stack, '\n', Mt*MAX);
     
     scanf("%d", &n);
     while(1){
@@ -52,12 +48,6 @@ int main(){
             scanf("%d %d", &n, &m);
             push(m, stack, pop(n, stack));
         }
-        /*
-        else if(strcmp(cmnd, "show")==0){
-            scanf("%d", &n);
-            show(n, stack);
-        }
-        */
     }
     for(i=0;i<j;i++){
         printf("%c\n", pops[i]);
