ed64aa7463ddeb0d0e1ad6380bdaf427fef4d768
wada811 <89.at.usi@gmail.com>
2012-05-19 21:40:17 +0900

    pass AOJ

--- vol0/AOJ0008.cpp
+++ vol0/AOJ0008.cpp
@@ -2,29 +2,36 @@
 //*
 #include <stdio.h>
 
-int main(){
-    long prime;
-    long i, j;
-    int count;
-    int flag;
+#define MAX 1000000
 
-    while(scanf("%ld", &prime) != EOF){
-        count = 0;
-        for(i = 3; i <= prime; i += 2){
-            flag = 0;
-            for(j = 3; j <= i / 2; j += 2){
-                if(i % j == 0){
-                    flag = 1;
-                    break;
-                }
-            }
-            if(flag == 0){
-                count++;
+int flag[MAX];
+
+int main(){
+    int prime;
+    int i, j;
+    int count;
+    
+    flag[0] = flag[1] = 0;
+    for(i = 2; i < MAX; i++){
+        flag[i] = 1;
+    }
+    
+    for(i = 2; i * i < MAX; i++){
+        if(flag[i]){
+            for(j = 2 * i; j < MAX; j+=i){
+                flag[j] = 0;
             }
         }
-
-        printf("%d\n", prime > 1 ? ++count : count);
     }
+
+    while(scanf("%d", &prime) != EOF){
+        count = 0;
+        while(prime){
+            count += flag[prime--];
+        }
+        printf("%d\n", count);
+    }
+
     return 0;
 }
 //*/
