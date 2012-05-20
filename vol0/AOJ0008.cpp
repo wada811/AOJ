52cfacebf51daf27816e24170aa4624f39c7e632
wada811 <89.at.usi@gmail.com>
2012-05-20 11:44:01 +0900

    add correct AOJ0008 and AOJ0009

--- vol0/AOJ0008.cpp
+++ vol0/AOJ0008.cpp
@@ -2,35 +2,24 @@
 //*
 #include <stdio.h>
 
-#define MAX 1000000
-
-int isPrime[MAX];
-
 int main(){
-    int num;
-    int i, j;
+    int n;
+    int a, b, c, d;
     int count;
-    
-    // 配列の初期化
-    isPrime[0] = isPrime[1] = 0;
-    for(i = 2; i < MAX; i++){
-        isPrime[i] = 1;
-    }
-    
-    // エラストテネスのふるい
-    for(i = 2; i * i < MAX; i++){
-        if(isPrime[i]){
-            // iの倍数は素数じゃないのでフラグ変更
-            for(j = 2 * i; j < MAX; j+=i){
-                isPrime[j] = 0;
-            }
-        }
-    }
-
-    while(scanf("%d", &num) != EOF){
+        
+    while(scanf("%d", &n) != EOF){
         count = 0;
-        while(num){
-            count += isPrime[num--];
+        for(a = 0; a < 10; a++){
+            for(b = 0; b < 10; b++){
+                for(c = 0; c < 10; c++){
+                    for(d = 0; d < 10; d++){
+                        if(a + b + c + d == n){
+                            count++;
+                            break;
+                        }
+                    }
+                }
+            }
         }
         printf("%d\n", count);
     }
